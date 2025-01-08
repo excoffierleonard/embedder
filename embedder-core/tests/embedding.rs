d1173d676c3b4b532df560c7afea8431ab952380
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-01-08 18:56:14 -0500

    comments

--- embedder-core/tests/embedding.rs
+++ embedder-core/tests/embedding.rs
@@ -7,11 +7,11 @@ async fn embed_ollama_success() {
     dotenv().ok();
 
     let texts = vec!["Hello, world!".to_string(), "Goodbye, world!".to_string()];
-
     let client = OllamaClient::new();
 
     let embeddings = InputTexts::new(texts).embed(client).await.unwrap();
 
+    // Check that the embeddings are returned
     assert_eq!(embeddings.len(), 2);
 }
 
@@ -21,10 +21,10 @@ async fn embed_openai_success() {
     let api_key = var("OPENAI_API_KEY").unwrap().to_string();
 
     let texts = vec!["Hello, world!".to_string(), "Goodbye, world!".to_string()];
-
     let client = OpenAIClient::new(api_key);
 
     let embeddings = InputTexts::new(texts).embed(client).await.unwrap();
 
+    // Check that the embeddings are returned
     assert_eq!(embeddings.len(), 2);
 }
