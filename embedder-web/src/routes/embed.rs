36202a6c8fba8f336140374ee2efa0de0798a168
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-01-09 18:42:41 -0500

    embedder openai and ollama works

--- embedder-web/src/routes/embed.rs
+++ embedder-web/src/routes/embed.rs
@@ -2,8 +2,14 @@
 
 use crate::{config::LocalConfig, errors::ApiError, responses::EmbedResponse};
 use actix_web::{http::header, post, web::Json};
-use embedder_core::{InputTexts, OllamaClient};
+use embedder_core::{InputTexts, OllamaClient, OpenAIClient};
 use serde::Deserialize;
+
+const OPENAI_EMBEDDING_MODELS: [&str; 3] = [
+    "text-embedding-ada-002",
+    "text-embedding-3-small",
+    "text-embedding-3-large",
+];
 
 #[derive(Deserialize)]
 struct EmbedRequestBody {
@@ -26,10 +32,45 @@ async fn embed_texts(
         .map(|auth| auth.trim_start_matches("Bearer ").to_string())
         .or(config.fallback_openai_api_key);
 
-    // Consider not using clone here
-    let client = OllamaClient::new(Some(model.clone()), Some(config.ollama_api_url));
+    // If the model is an openai model then use the openai api key and client, if none, then fallback to the ollama client
+
+    // TODO: Consider not using clone here but dont fall into liftime hell
+    let embeddings = match (
+        OPENAI_EMBEDDING_MODELS.contains(&model.as_str()),
+        openai_api_key,
+    ) {
+        (true, Some(api_key)) => embed_with_openai(api_key, model.clone(), texts).await?,
+        (true, None) => {
+            return Err(ApiError::BadRequest(
+                "OpenAI API key is required".to_string(),
+            ))
+        }
+        (false, _) => embed_with_ollama(config.ollama_api_url, model.clone(), texts).await?,
+    };
+
+    Ok(EmbedResponse { model, embeddings })
+}
+
+async fn embed_with_ollama(
+    url: String,
+    model: String,
+    texts: Vec<String>,
+) -> Result<Vec<Vec<f32>>, ApiError> {
+    let client = OllamaClient::new(Some(model), Some(url));
 
     let embeddings = InputTexts::new(texts).embed(client).await?;
 
-    Ok(EmbedResponse { model, embeddings })
+    Ok(embeddings)
+}
+
+async fn embed_with_openai(
+    api_key: String,
+    model: String,
+    texts: Vec<String>,
+) -> Result<Vec<Vec<f32>>, ApiError> {
+    let client = OpenAIClient::new(api_key, Some(model));
+
+    let embeddings = InputTexts::new(texts).embed(client).await?;
+
+    Ok(embeddings)
 }
--- embedder-web/tests/endpoints.rs
+++ embedder-web/tests/endpoints.rs
@@ -1,6 +1,8 @@
 use actix_web::{test, App};
+use dotenv::dotenv;
 use embedder_web::routes::embed_texts;
 use serde::{Deserialize, Serialize};
+use std::env::var;
 
 #[derive(Serialize)]
 struct EmbedRequestBody {
@@ -22,7 +24,6 @@ async fn request_embed_ollama_success() {
     // Create body
     let model = "nomic-embed-text".to_string();
     let texts = vec!["Hello, world!".to_string(), "Goodbye, world!".to_string()];
-
     let body = EmbedRequestBody {
         model: model.clone(),
         texts: texts.clone(),
@@ -47,7 +48,41 @@ async fn request_embed_ollama_success() {
 }
 
 #[actix_web::test]
-async fn request_parse_openai_custom_success() {}
+async fn request_parse_openai_custom_success() {
+    dotenv().ok();
+    // Setup
+    let app = test::init_service(App::new().service(embed_texts)).await;
+
+    // Create Header
+    let openai_api_key = var("OPENAI_API_KEY").unwrap();
+    let auth_header = ("Authorization", format!("Bearer {}", openai_api_key));
+
+    // Create body
+    let model = "text-embedding-3-large".to_string();
+    let texts = vec!["Hello, world!".to_string(), "Goodbye, world!".to_string()];
+    let body = EmbedRequestBody {
+        model: model.clone(),
+        texts: texts.clone(),
+    };
+
+    // Create request
+    let req = test::TestRequest::post()
+        .uri("/embed")
+        .insert_header(auth_header)
+        .set_json(&body)
+        .to_request();
+
+    // Get response
+    let resp = test::call_service(&app, req).await;
+
+    // Assert the results
+    let status = resp.status();
+    assert!(status.is_success());
+
+    let body: EmbedResponseBody = test::read_body_json(resp).await;
+    assert_eq!(body.model, model);
+    assert_eq!(body.embeddings.len(), texts.len());
+}
 
 #[actix_web::test]
 async fn request_parse_openai_default_success() {}
