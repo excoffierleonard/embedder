6fdd0c8eefbb0959cff2cfe9b42c2b59a9532bed
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-01-09 18:43:36 -0500

    fallback test passed

--- embedder-web/tests/endpoints.rs
+++ embedder-web/tests/endpoints.rs
@@ -85,4 +85,33 @@ async fn request_parse_openai_custom_success() {
 }
 
 #[actix_web::test]
-async fn request_parse_openai_default_success() {}
+async fn request_parse_openai_fallback_success() {
+    dotenv().ok();
+    // Setup
+    let app = test::init_service(App::new().service(embed_texts)).await;
+
+    // Create body
+    let model = "text-embedding-3-large".to_string();
+    let texts = vec!["Hello, world!".to_string(), "Goodbye, world!".to_string()];
+    let body = EmbedRequestBody {
+        model: model.clone(),
+        texts: texts.clone(),
+    };
+
+    // Create request
+    let req = test::TestRequest::post()
+        .uri("/embed")
+        .set_json(&body)
+        .to_request();
+
+    // Get response
+    let resp = test::call_service(&app, req).await;
+
+    // Assert the results
+    let status = resp.status();
+    assert!(status.is_success());
+
+    let body: EmbedResponseBody = test::read_body_json(resp).await;
+    assert_eq!(body.model, model);
+    assert_eq!(body.embeddings.len(), texts.len());
+}
